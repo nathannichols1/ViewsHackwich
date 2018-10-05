b00f8573af640b85420afc63a0f5ff536cdec0b1
NATHAN NICHOLS <rl235817@rhs-macair-40.rlas-116.org>
2018-10-05 09:52:40 -0500

    Initial Commit

---
+++ ViewsHackwich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewsHackwich
+//
+//  Created by NATHAN NICHOLS on 10/5/18.
+//  Copyright © 2018 NATHAN NICHOLS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
